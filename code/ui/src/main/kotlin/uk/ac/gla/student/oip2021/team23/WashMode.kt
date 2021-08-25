e8e84fb20d719de317f572807759bb37ebabd38c
A Flying Poro <12255407+A-Flying-Poro@users.noreply.github.com>
2021-08-26 03:48:33 +0800

    Added Progress screen

--- code/ui/src/main/kotlin/uk/ac/gla/student/oip2021/team23/WashMode.kt
+++ code/ui/src/main/kotlin/uk/ac/gla/student/oip2021/team23/WashMode.kt
@@ -1,7 +1,7 @@
 package uk.ac.gla.student.oip2021.team23
 
-enum class WashMode {
-    WASH,
-    DRY,
-    SELF_CLEAN
+enum class WashMode(durationMin: Int) {
+    WASH(30),
+    DRY(15),
+    SELF_CLEAN(15)
 }
---
+++ code/ui/src/main/kotlin/uk/ac/gla/student/oip2021/team23/gui/GuiProgress.java
@@ -0,0 +1,16 @@
+package uk.ac.gla.student.oip2021.team23.gui;
+
+import javax.swing.*;
+
+public class GuiProgress {
+    private JPanel panelMain;
+    private JButton buttonStop;
+    private JLabel labelProgress;
+
+    public void start() {
+    }
+
+    public JPanel getMainPanel() {
+        return panelMain;
+    }
+}
