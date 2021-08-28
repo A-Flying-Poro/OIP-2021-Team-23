a3850c6ab77ccb62b50e33e93886dda6fe7ed358
A Flying Poro <12255407+A-Flying-Poro@users.noreply.github.com>
2021-08-29 04:40:34 +0800

    Updated sequences

--- code/ui/src/main/kotlin/uk/ac/gla/student/oip2021/team23/sequence/Sequence.kt
+++ code/ui/src/main/kotlin/uk/ac/gla/student/oip2021/team23/sequence/Sequence.kt
@@ -4,9 +4,25 @@ import uk.ac.gla.student.oip2021.team23.interf.InterfaceHelper
 
 data class Sequence(
     val name: String,
+    val displayText: String,
     /** Estimated time in minutes */
     val time: Int,
     val requireAcknowledgement: Boolean,
-    var runnable: Runnable? = null,
-    val outputState: InterfaceHelper.State = InterfaceHelper.State.NONE
-)
+    val outputState: InterfaceHelper.State = InterfaceHelper.State.NONE,
+    val repeatable: Repeatable = Repeatable.NONE,
+    val maxRepeatCount: Int = 0,
+
+    val userPrompt: PromptType? = null,
+    val userPromptMessage: String? = null
+) {
+    enum class PromptType {
+        WARNING,
+        INFO,
+        SUCCESS
+    }
+
+    enum class Repeatable {
+        NONE,
+        DRY
+    }
+}
--- code/ui/src/main/kotlin/uk/ac/gla/student/oip2021/team23/sequence/WashSequences.kt
+++ code/ui/src/main/kotlin/uk/ac/gla/student/oip2021/team23/sequence/WashSequences.kt
@@ -7,59 +7,92 @@ enum class WashSequences(val sequences: List<Sequence>) {
         listOf(
             Sequence(
                 "Fill",
+                "Filling...",
                 5,
                 true,
                 outputState = InterfaceHelper.State.FILL
             ),
             Sequence(
                 "Soak",
+                "Soaking...",
                 1,
                 false
             ),
             Sequence(
                 "Reduce Temperature",
+                "Reducing temperature...",
                 1,
                 true,
                 outputState = InterfaceHelper.State.REDUCE_TEMP
             ),
             Sequence(
                 "Request User for Ultrasonic Cleaner",
+                "Requesting user input...",
                 0,
                 false,
-                runnable = {
-                    // Request user to turn on ultrasonic cleaner
-                }
+                userPrompt = Sequence.PromptType.INFO,
+                userPromptMessage = "Please press start on the ultrasonic cleaner."
             ),
             Sequence(
                 "Ultrasonic Wash",
+                "Washing...",
                 1,
                 false,
                 outputState = InterfaceHelper.State.WASH
             ),
             Sequence(
+                "Request User to stop Ultrasonic Cleaner",
+                "Requesting user input...",
+                0,
+                false,
+                userPrompt = Sequence.PromptType.INFO,
+                userPromptMessage = "Please press stop on the ultrasonic cleaner."
+            ),
+            Sequence(
                 "Drain",
+                "Draining...",
                 1,
                 false,
                 outputState = InterfaceHelper.State.DRAIN
             ),
             Sequence(
                 "Rinse",
+                "Rinsing...",
                 10,
                 true,
                 outputState = InterfaceHelper.State.RINSE
             ),
             Sequence(
-                "Drying",
-                1,
+                "Dry",
+                "Drying...",
+                15,
                 true,
-                outputState = InterfaceHelper.State.DRY
+                outputState = InterfaceHelper.State.DRY,
+                repeatable = Sequence.Repeatable.DRY,
+                maxRepeatCount = 3
             ),
             Sequence(
                 "Buzzer",
+                "Complete!",
                 time = 0,
                 false,
-                outputState = InterfaceHelper.State.ALERT
+                outputState = InterfaceHelper.State.ALERT,
+                userPrompt = Sequence.PromptType.SUCCESS,
+                userPromptMessage = "COMPLETE"
             )
         )
-    )
+    );
+
+    companion object {
+        @JvmStatic
+        val dryRepeatable = Sequence(
+            "Dry",
+            "Dying...",
+            5,
+            true,
+            outputState = InterfaceHelper.State.DRY,
+            repeatable = Sequence.Repeatable.DRY,
+            maxRepeatCount = 3
+        )
+    }
 }
